9a99d9f45e889806f71a639063e535c3eba99cbb
Yan Zhabko <113374407+yanzhabko@users.noreply.github.com>
2024-03-22 13:40:07 +0100

    init

---
+++ components/FooterComponent.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const FooterComponent = () => {
+  return (
+    <footer>
+    </footer>
+  );
+};
+
+export default FooterComponent;
---
+++ components/HeaderComponent.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const HeaderComponent = () => {
+  return (
+    <header>
+    </header>
+  );
+};
+
+export default HeaderComponent;
