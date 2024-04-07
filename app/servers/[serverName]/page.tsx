5e308cff40812a4e680a8cebb23efaadd68350ce
Yan Zhabko <113374407+yanzhabko@users.noreply.github.com>
2024-04-08 00:26:06 +0200

    Update page.tsx

--- app/servers/[serverName]/page.tsx
+++ app/servers/[serverName]/page.tsx
@@ -1,4 +1,14 @@
-export default function Servers({ params }: { params: any }) {
+import React from "react";
+
+type ServersProps = {
+  params: {
+    serverName: string;
+  };
+};
+
+const Servers: React.FC<ServersProps> = ({ params }) => {
   console.log(params);
   return <div>{params.serverName}</div>;
-}
+};
+
+export default Servers;
